a3a8f623f60e69aaca0b77233d79867d8459f716
johnstat101 <johnjorogek@gmail.com>
2022-03-14 01:34:50 +0300

    add project models

--- myapps/models.py
+++ myapps/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Projects(models.Model):
+    project_name = models.CharField(max_length=100)
+
+
+class Profile(models.Model):
+    username = models.CharField(max_length=50)
+    bio = models.CharField(max_length=200)
+    projects = models.ManyToManyField(Projects)
+    profile_picture = models.ImageField(upload_to = 'images/',blank = True)
--- projectreviews/settings.py
+++ projectreviews/settings.py
@@ -64,8 +64,10 @@ WSGI_APPLICATION = 'projectreviews.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'projectreviews',
+        'USER': 'postgres',
+    'PASSWORD':'Njoro',
     }
 }
 
